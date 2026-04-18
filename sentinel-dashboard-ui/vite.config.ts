dea68c45eed4c8b5c44a9561308b59871ea44880
dongjiahao-jk <dongjiahao-jk@qifu.com>
2026-04-18 12:27:29 +0800

    bug fixed

--- sentinel-dashboard-ui/vite.config.ts
+++ sentinel-dashboard-ui/vite.config.ts
@@ -13,10 +13,57 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": {
+      "/v1": {
         target: "http://localhost:8080",
         changeOrigin: true,
-        rewrite: (p) => p.replace(/^\/api/, ""),
+      },
+      "/v2": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/auth": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/metric": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/resource": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/app": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/cluster": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/gateway": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/system": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/degrade": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/authority": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/paramFlow": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/registry": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
       },
     },
   },
